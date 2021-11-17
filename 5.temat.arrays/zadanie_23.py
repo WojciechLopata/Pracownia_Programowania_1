a1625aa12042553821ff762e65658ab966220f01
WojciechLopata <91980175+WojciechLopata@users.noreply.github.com>
2021-11-17 11:55:33 +0100

    Update zadanie_23.py

--- 5.temat.arrays/zadanie_23.py
+++ 5.temat.arrays/zadanie_23.py
@@ -1,5 +1,7 @@
+Import bubblesort
+
 def median(array):
     if(len(array)%2==0):return (array[len(array)//2]+array[(len(array)//2)-1])/2
     else: return array[(len(array)//2)]
 array=[1,2,3,4]
-print(median(array))
+print(median(bubblesort.bubblesort(array)))
